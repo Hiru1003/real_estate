488926262710adc3206ffa622d65c560226e28ea
Hiru1003 <hirumiperera10@gmail.com>
2024-04-29 14:26:48 +0530

    feat: routes

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import authRouter from "./routes/auth.route.js";
+import postRouter from "./routes/post.route.js";
 
 const app = express();
 
-app.use("/api/test", (req,res) => {
-    res.send("It works")  
-})
+app.use("api/posts", postRouter);
+app.use("api/auth", authRouter);
+
 
 app.listen(8000, () => {
     console.log("Server is running")
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,23 @@
 import express from "express"
 
-const router =express.Router()
+const router =express.Router();
 
 
-router.get("/test" , (req,res) =>{
-    console.log("Router works")
-})
+// router.get("/test" , (req,res) =>{
+//     console.log("Router works")
+// })
+
+// router.post("/test" , (req,res) =>{
+//     console.log("Router works")
+// })
+
+// router.put("/test" , (req,res) =>{
+//     console.log("Router works")
+// })
+
+// router.delete("/test" , (req,res) =>{
+//     console.log("Router works")
+// })
+
 
 export default router
