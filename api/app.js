dfeb276ef27ee84ad3cd15a38fe3a7d5e8306ee5
Hiru1003 <hirumiperera10@gmail.com>
2024-04-30 09:32:17 +0530

    feat: create use with res and req

--- api/app.js
+++ api/app.js
@@ -1,13 +1,13 @@
 import express from "express";
-import authRouter from "./routes/auth.route.js";
-import postRouter from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
 
 const app = express();
 
 app.use(express.json())
 
-app.use("api/posts", postRouter);
-app.use("api/auth", authRouter);
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
 
 
 app.listen(8000, () => {
