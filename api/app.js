d7e60bb35c25b33e72a4169fed51ce53a0d2a341
Hiru1003 <hirumiperera10@gmail.com>
2024-05-02 09:16:11 +0530

    feat: cookieParse

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
@@ -17,3 +19,4 @@ app.listen(8000, () => {
 
 
 //console-ninja node --watch app.js
+//console-ninja node --env-file .env  --watch app.js
